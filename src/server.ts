5ca8ff188cc04a81f628330acec7918258172dd5
viniciussillva98 <viniciussaldanha1998@gmail.com>
2026-03-03 19:51:45 -0300

    config-server

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ const Port = env.PORT;
 initializeFirebaseAdmin();
 
 const StartServer = async () => {
+  console.log("DATABASE_URL:", !!process.env.DATABASE_URL);
+  console.log("PROJECT_ID:", !!process.env.FIREBASE_PROJECT_ID);
+  console.log("CLIENT_EMAIL:", !!process.env.FIREBASE_CLIENT_EMAIL);
+  console.log("PRIVATE_KEY:", !!process.env.FIREBASE_PRIVATE_KEY);
   try {
     await prismaConnect();
 
