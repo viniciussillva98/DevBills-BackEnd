cf4d249f7454ee05401f3b6f352fc60d0c9f330b
viniciussillva98 <viniciussaldanha1998@gmail.com>
2026-03-03 19:47:57 -0300

    alteração.api.index

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+
 import app from "../src/app";
 import initializeFirebaseAdmin from "../src/config/firebase";
 import { prismaConnect } from "../src/config/prisma";
@@ -6,30 +7,12 @@ import { initializeGlobalCategories } from "../src/services/globalCategories.ser
 let initialized = false;
 
 export default async function handler(req: any, res: any) {
-    try {
-        if (!initialized) {
-            console.log("Inicializando backend...");
-
-            initializeFirebaseAdmin();
-            console.log("Firebase OK");
-
-            await prismaConnect();
-            console.log("Prisma OK");
-
-            await initializeGlobalCategories();
-            console.log("Categorias OK");
-
-            initialized = true;
-        }
-
-        await app.ready();
-        app.server.emit("request", req, res);
-
-    } catch (error) {
-        console.error("ERRO NO BACKEND:", error);
-        res.status(500).json({
-            message: "Erro interno",
-            error: String(error),
-        });
+    if (!initialized) {
+        initializeFirebaseAdmin();
+        await prismaConnect();
+        await initializeGlobalCategories();
+        initialized = true;
     }
+
+    return app.ready().then(() => app.server.emit('request', req, res));
 }
